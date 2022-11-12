6e9329248b0393093397dd1166ff4920c311ba90
Eryk Możdżeń <ermozdzen@gmail.com>
2022-11-12 18:23:13 +0100

    add HAL as submodule

--- stm32-hal-rtos-minimal/app/src/main.c
+++ stm32-hal-rtos-minimal/app/src/main.c
@@ -15,10 +15,6 @@ void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName) {
 	while(1);
 }
 
-void vApplicationTickHook() {
-	HAL_IncTick();
-}
-
 void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize) {
     static StaticTask_t xIdleTaskTCB;
     static StackType_t uxIdleTaskStack[configMINIMAL_STACK_SIZE];
@@ -28,25 +24,36 @@ void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer, StackTyp
     *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
 }
 
+void vApplicationTickHook() {
+	HAL_IncTick();
+}
+
 void clock_init() {
-	RCC_OscInitTypeDef oscillator = {0};
-	oscillator.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-	oscillator.HSIState = RCC_HSI_ON;
-	oscillator.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-	oscillator.PLL.PLLState = RCC_PLL_ON;
-	oscillator.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-	oscillator.PLL.PLLM = 8;
-	oscillator.PLL.PLLN = 100;
-	oscillator.PLL.PLLP = 2;
-	oscillator.PLL.PLLQ = 4;
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+	RCC_OscInitTypeDef oscillator = {
+		.OscillatorType = RCC_OSCILLATORTYPE_HSI,
+		.HSIState = RCC_HSI_ON,
+		.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT,
+		.PLL.PLLState = RCC_PLL_ON,
+		.PLL.PLLSource = RCC_PLLSOURCE_HSI,
+		.PLL.PLLM = 8,
+		.PLL.PLLN = 100,
+		.PLL.PLLP = 2,
+		.PLL.PLLQ = 4
+	};
+
 	HAL_RCC_OscConfig(&oscillator);
 
-	RCC_ClkInitTypeDef clock = {0};
-	clock.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-	clock.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	clock.AHBCLKDivider = RCC_SYSCLK_DIV1;
-	clock.APB1CLKDivider = RCC_HCLK_DIV2;
-	clock.APB2CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitTypeDef clock = {
+		.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2,
+		.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK,
+		.AHBCLKDivider = RCC_SYSCLK_DIV1,
+		.APB1CLKDivider = RCC_HCLK_DIV2,
+		.APB2CLKDivider = RCC_HCLK_DIV1
+	};
+
 	HAL_RCC_ClockConfig(&clock, FLASH_LATENCY_2);
 }
 
