9aaf858d88216daf3b5c65f654f332a2d93b789e
Eryk Możdżeń <ermozdzen@gmail.com>
2023-01-06 22:13:34 +0100

    add minimal project to esp8266

---
+++ esp8266-rtos-sdk/main/app_main.c
@@ -0,0 +1,31 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+static void blink(void *param) {
+	(void)param;
+
+	gpio_config_t led = {
+		.pin_bit_mask = GPIO_Pin_2,
+		.mode = GPIO_MODE_OUTPUT,
+		.pull_up_en = GPIO_PULLUP_DISABLE,
+		.pull_down_en = GPIO_PULLDOWN_DISABLE,
+		.intr_type = GPIO_INTR_DISABLE
+	};
+
+	gpio_config(&led);
+
+	while(1) {
+
+		gpio_set_level(GPIO_NUM_2, 1);
+		vTaskDelay(100);
+
+		gpio_set_level(GPIO_NUM_2, 0);
+		vTaskDelay(100);
+	}
+}
+
+void app_main() {
+	
+	xTaskCreate(blink, "blink", 3000, NULL, 5, NULL);
+}
