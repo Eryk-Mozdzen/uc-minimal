bd70a015681a090e70651cb5dcf4e04cbbcf2842
Eryk Możdżeń <ermozdzen@gmail.com>
2023-07-05 21:24:14 +0200

    working blink in ASM

--- stm32-asm/src/main.s
+++ stm32-asm/src/main.s
@@ -10,7 +10,43 @@ vtable:
 .type reset_handler, %function
 .size reset_handler, .-reset_handler
 reset_handler:
-    mov r0, #1
+	ldr r0, =0x40023830
+    ldr r1, [r0]
+	ldr r2, =0x01
+    orr r1, r1, r2
+    str r1, [r0]
+
+	ldr r0, =0x40020000
+    ldr r1, [r0]
+	ldr r2, =0x400
+    orr r1, r1, r2
+    str r1, [r0]
 
 main_loop:
+	ldr r0, =0x40020018
+    ldr r1, [r0]
+	ldr r2, =0x20
+    orr r1, r1, r2
+    str r1, [r0]
+
+	ldr r0, =1000000
+	bl delay
+
+	ldr r0, =0x40020018
+    ldr r1, [r0]
+	ldr r2, =0x200000
+    orr r1, r1, r2
+    str r1, [r0]
+
+	ldr r0, =1000000
+	bl delay
+
     b main_loop
+
+delay:
+    push {lr}
+delay_loop:
+    sub r0, #1
+	cmp r0, #0
+    bne delay_loop
+    pop {pc}
