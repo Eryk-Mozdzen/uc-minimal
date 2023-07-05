d74425a417b49822e1e76727c7658c1674576504
Eryk Możdżeń <ermozdzen@gmail.com>
2023-07-05 15:50:47 +0200

    update c++ example

--- stm32-cpp/src/main.cpp
+++ stm32-cpp/src/main.cpp
@@ -5,7 +5,7 @@ int main() {
     RCC->AHB1ENR |=RCC_AHB1ENR_GPIOAEN;
 	GPIOA->MODER |=GPIO_MODER_MODER5_0;
 
-    while(1) {
+    while(true) {
         GPIOA->BSRR |=GPIO_BSRR_BS5;
         for(uint32_t i=0; i<1000000; ++i);
 
