b851155240507daf67fb3bd6471652c85e13ffdc
Eryk Możdżeń <ermozdzen@gmail.com>
2023-07-08 01:32:47 +0200

    update Zephyr example

--- stm32-c-zephyr/app/src/main.c
+++ stm32-c-zephyr/app/src/main.c
@@ -2,26 +2,17 @@
 #include <zephyr/drivers/gpio.h>
 
 static void blink() {
-	
 	const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 
-	if(!device_is_ready(led.port))
-		return;
-
-	if(gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE)<0)
-		return;
+	gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 
 	while(1) {
+		gpio_pin_set_dt(&led, 1);
+		k_msleep(1000);
 
-		if(gpio_pin_toggle_dt(&led)<0)
-			return;
-
-		k_msleep(100);
+		gpio_pin_set_dt(&led, 0);
+		k_msleep(1000);
 	}
 }
 
 K_THREAD_DEFINE(blink_id, 1024, blink, NULL, NULL, NULL, 7, 0, 0);
-
-void main() {
-
-}
